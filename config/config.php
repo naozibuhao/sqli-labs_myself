fd00bebab6a61d5e649806c9af383339034ccf47
naozibuhao <o1313113@qq.com>
2020-04-26 14:12:24 +0800

    添加学习过程中sql语句打印

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * @Author: your name
+ * @Date: 2020-04-26 13:20:58
+ * @LastEditTime: 2020-04-26 13:29:06
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \sqli-labs\config\config.php
+ */
+
+$flagecho = true; // 是否打印sql语句 2020-4-26 By wyyw
+if($flagecho){
+    echo $sql;
+    echo "<br>";
+}
+
+?>
